2d32bbbfbf7ef359314288b21056daeb8a4b077d
chaitalipatel92 <chaitalipatel92@gmail.com>
2016-05-24 21:50:04 -0400

    Database Summer Project
    Finds the shortest path to reach from source to destination including
    bus path and walk path

---
+++ src/java/Model/BusTravel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+public class BusTravel {
+    int busTravelId;
+    int pathId;
+    private String startsAt;
+    private String endsAt;
+    private double distance;
+    private short travelTime;
+    String busId;
+    private String busName;
+}
---
+++ src/java/Util/Util.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Util;
+public class Util {
+    
+    public static final String URL = "jdbc:mysql://localhost:3306/tau?zeroDateTimeBehavior=convertToNull";
+    public static final String username = "root";
+    public static final String password = "";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    
+}
